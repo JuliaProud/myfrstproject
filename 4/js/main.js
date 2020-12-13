79ea8805722bfd261126c47a313b3440e7c13476
JuliaProud <62939991+JuliaProud@users.noreply.github.com>
2020-12-13 19:53:24 +0200

    загрузила первый сайт

---
+++ 4/js/main.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+  $('.single-item-rtl').slick({
+  dots: true,
+  speed: 500,
+  autoplay:true,
+  infinite:true,
+  dotsClass:'slick-dots',
+  prevArrow:'none',
+  nextArrow:' none',
+  });
+});
